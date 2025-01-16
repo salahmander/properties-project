30cb0a52cdf412f561553894ced5312c2cf58fe6
salahmander <salahabdo@hotmail.co.uk>
2025-01-16 17:54:27 +0000

    feat: conditionally render user menu

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -9,12 +9,14 @@ import { usePathname } from "next/navigation";
 import logo from "@/assets/images/logo-white.png";
 
 import { FaGoogle } from "react-icons/fa";
+
 import MobileMenu from "./MobileMenu/MobileMenu";
-import ProfileDropdown from "./ProfileDropdown/ProfileDropdown";
 import DesktopMenu from "./DesktopMenu/DesktopMenu";
+import UserMenu from "./UserMenu/UserMenu";
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const pathName = usePathname();
 
@@ -62,47 +64,18 @@ const Navbar = () => {
             <DesktopMenu pathName={pathName} />
           </div>
 
-          {/* Right Side Menu (Logged Out) */}
-          <div className="hidden md:block md:ml-6">
-            <div className="flex items-center">
-              <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
-                <FaGoogle className="text-white mr-2" />
-                <span>Login or Register</span>
-              </button>
+          {!isLoggedIn && (
+            <div className="hidden md:block md:ml-6">
+              <div className="flex items-center">
+                <button className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  <FaGoogle className="text-white mr-2" />
+                  <span>Login or Register</span>
+                </button>
+              </div>
             </div>
-          </div>
+          )}
 
-          {/* Right Side Menu (Logged In) */}
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0">
-            <Link href="/messages" className="relative group">
-              <button
-                type="button"
-                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-              </button>
-              <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-                2
-              </span>
-            </Link>
-
-            <ProfileDropdown />
-          </div>
+          {isLoggedIn && <UserMenu />}
         </div>
       </div>
 
