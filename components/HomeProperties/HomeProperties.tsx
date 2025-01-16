3319d4bf5aa16a406f53bcc8d7cd22184e10c017
salahmander <salahabdo@hotmail.co.uk>
2025-01-16 21:20:41 +0000

    feat: create new home properties component, and render 3 most recent properies

---
+++ components/HomeProperties/HomeProperties.tsx
@@ -0,0 +1,28 @@
+import properties from "@/properties.json";
+import PropertyCard from "../PropertyCard/PropertyCard";
+
+const HomeProperties = () => {
+  const recentProperties = properties.slice(0, 3);
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h2 className="text-3xl font-bold text-blue-500 mb-6">
+            Recent Properties
+          </h2>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {recentProperties.map((property) => (
+                <PropertyCard key={property._id} {...property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomeProperties;
