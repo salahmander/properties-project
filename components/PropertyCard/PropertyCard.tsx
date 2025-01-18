5f12959e049bb43426eec4086d131726fa8b11b4
salahmander <salahabdo@hotmail.co.uk>
2025-01-18 14:37:46 +0000

    fix: redirect to property page details is clicked

--- components/PropertyCard/PropertyCard.tsx
+++ components/PropertyCard/PropertyCard.tsx
@@ -87,7 +87,7 @@ const PropertyCard = (property: PropertyType) => {
             </span>
           </div>
           <Link
-            href="/property"
+            href={`/properties/${property._id}`}
             className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
