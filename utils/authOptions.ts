3682e7e4516d78deaaee5d4ebb280670c37a36c5
salahmander <salahabdo@hotmail.co.uk>
2025-01-19 00:40:59 +0000

    doc: create docs for both getSesion and authOption utils

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -4,6 +4,29 @@ import connectDB from "@/config/database";
 import User from "@/models/User";
 import type { CustomSession, GoogleProfileType } from "@/types/index.types";
 
+/**
+ * Authentication options for the application.
+ * 
+ * @property {Array} providers - List of authentication providers.
+ * @property {Object} callbacks - Callback functions for authentication events.
+ * 
+ * @example
+ * // Example usage of authOptions
+ * import { authOptions } from './utils/authOptions';
+ * 
+ * @typedef {Object} GoogleProfileType - Type definition for Google profile.
+ * @typedef {Object} CustomSession - Type definition for custom session.
+ * 
+ * @callback signIn
+ * @param {Object} params - Parameters for the signIn callback.
+ * @param {GoogleProfileType | undefined} params.profile - Google profile information.
+ * @returns {Promise<boolean>} - Returns true if sign in is successful.
+ * 
+ * @callback session
+ * @param {Object} params - Parameters for the session callback.
+ * @param {CustomSession} params.session - Custom session object.
+ * @returns {Promise<CustomSession>} - Returns the modified session object.
+ */
 export const authOptions = {
   providers: [
     GoogleProvider({
--- utils/getSession.ts
+++ utils/getSession.ts
@@ -2,6 +2,15 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/utils/authOptions";
 import { CustomSession } from "@/types/index.types";
 
+/**
+ * Retrieves the current session user.
+ *
+ * This function fetches the current session using the `getServerSession` function
+ * and returns an object containing the user and userId if the session and user exist.
+ * If the session or user does not exist, it returns null.
+ *
+ * @returns {Promise<{ user: CustomUser; userId: string } | null>} An object containing the user and userId, or null if no session or user is found.
+ */
 export const getSessionUser = async () => {
   const session = (await getServerSession(authOptions as any)) as CustomSession; //TODO: fix any
 
