6519e0bda1406d86aaf1728b288c4b12ad6e6567
salahmander <salahabdo@hotmail.co.uk>
2025-01-23 17:19:15 +0000

    feat: add mapbox and pin location of property onto the map

--- components/Property/PropertyMap/PropertyMap.tsx
+++ components/Property/PropertyMap/PropertyMap.tsx
@@ -1,17 +1,26 @@
 "use client";
 
+import Image from "next/image";
+
 import { useEffect, useState } from "react";
 import { setDefaults, fromAddress, OutputFormat } from "react-geocode";
+import Map, { Marker } from "react-map-gl";
+
+import pin from "@/assets/images/pin.svg";
+
+import Spinner from "@/components/Spinner/Spinner";
 
 import type { PropertyType } from "@/types/properties.types";
+
+import "mapbox-gl/dist/mapbox-gl.css";
 
 type PropertyMapProps = {
   property: PropertyType;
 };
 
 const PropertyMap = ({ property }: PropertyMapProps) => {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
   const [viewport, setViewport] = useState({
     latitude: 0,
     longitude: 0,
@@ -57,13 +66,28 @@ const PropertyMap = ({ property }: PropertyMapProps) => {
     fetchCords();
   }, []);
 
-  if (loading) return <h3>Loading...</h3>;
-  if (geocodeError) return <div className="text-xl">No location data found</div>
+  if (loading) return <Spinner />;
+
+  if (geocodeError)
+    return <div className="text-xl">No location data found</div>;
 
   return (
-    <div>
-      <h1>Property Map</h1>
-    </div>
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 14,
+        }}
+        style={{ width: "100%", height: 500 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={lng} latitude={lat} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
   );
 };
 
