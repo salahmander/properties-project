bc066fe6dd820dd00abdc7029aec2a0f42427444
salahmander <salahabdo@hotmail.co.uk>
2025-01-24 00:44:24 +0000

    feat: create pagination component and implement it to properties page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -4,25 +4,57 @@ import Property from "@/models/Property";
 import PropertyCard from "@/components/PropertyCard/PropertyCard";
 
 import type { PropertiesType } from "@/types/properties.types";
+import Pagination from "@/components/Pagination/Pagination";
+import { parse } from "path";
 
-const PropertiesPage = async () => {
+type PropertiesPageProps = {
+  searchParams: {
+    pageSize: string;
+    page: string;
+  };
+};
+
+const PropertiesPage = async ({ searchParams }: PropertiesPageProps) => {
   await connectDB();
-  const properties = await Property.find({}).lean() as unknown as PropertiesType;
+
+  const { pageSize = "9", page = "1" } = await searchParams;
+
+  const pageSizeInt = parseInt(pageSize)
+  const pageInt = parseInt(page)
+
+  const skip = (pageInt - 1) * pageSizeInt;
+
+  const total = await Property.countDocuments({});
+  const properties = (await Property.find({})
+    .skip(skip)
+    .limit(pageSizeInt)
+    .lean()) as unknown as PropertiesType;
+
+  // Calculate if pagination is needed
+  const showPagination = total > pageSizeInt;
 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto px-4 py-6">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id.toString()} {...property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h1 className="text-2xl mb-4">Browse Properties</h1>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard {...property} key={property._id.toString()} />
+              ))}
+            </div>
+          )}
+          {showPagination && (
+            <Pagination
+              page={pageInt}
+              pageSize={pageSizeInt}
+              totalProperties={total}
+            />
+          )}
+        </div>
+      </section>
   );
 };
 
